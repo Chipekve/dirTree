3654a95beb9f5bc65cbefcf9061b3bb4ec920e0f
Egor <nikitin.egor@yahoo.com>
2026-04-06 04:24:11 +0600

    добавил отступы

--- main.go
+++ main.go
@@ -1,32 +1,36 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "os"
+	"fmt"
+	"log"
+	"os"
 )
 
-
 func main() {
-    entries, err := os.ReadDir(".")
-    if err != nil {
-        log.Fatal(err)
-    }
-    for _, e := range entries {
-        info, err := e.Info()
-        if err != nil {
-            continue
-        }
 
-        size := info.Size() / 1024
-        var t string
+	entries, err := os.ReadDir(".")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, e := range entries {
+		info, err := e.Info()
+		if err != nil {
+			continue
+		}
 
-        if e.IsDir() {
-            t = "dir"
-        } else {
-            t = "file"
-        }
+		var t string
+		var size string
 
-        fmt.Printf("%s %s %d KB\n", e.Name(), t, size)
-    }
+		if e.IsDir() {
+			size = "-"
+		} else {
+			size = fmt.Sprintf("%d KB", info.Size()/1024)
+		}
+		if e.IsDir() {
+			t = "dir"
+		} else {
+			t = "file"
+		}
+		fmt.Printf("%-10s %-5s %s\n", e.Name(), t, size)
+	}
 }
