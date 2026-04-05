1b92692865cff81d90efb1a74025cb0672977ec7
Egor <nikitin.egor@yahoo.com>
2026-04-06 04:57:49 +0600

    функции рекурсии и обычный readDir разделены

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
 )
 
-func main() {
-
-	entries, err := os.ReadDir(".")
+func printDirRecursive(path, indent string) {
+	entries, err := os.ReadDir(path)
 	if err != nil {
-		log.Fatal(err)
+		return
 	}
 	for _, e := range entries {
 		info, err := e.Info()
@@ -19,12 +19,47 @@ func main() {
 		}
 
 		t := "file"
-		size := fmt.Sprintf("%d KB", info.Size()/1024)
+		size := fmt.Sprintf("(%db)", info.Size()/1024)
 
 		if e.IsDir() {
 			t = "dir"
 			size = "-"
 		}
-		fmt.Printf("%-10s %-5s %s\n", e.Name(), t, size)
+		fmt.Printf("%s %-10s %s %-5s\n", indent, e.Name(), t, size)
+		if e.IsDir() {
+			printDirRecursive(path+"/"+e.Name(), indent+"  /")
+		}
+	}
+}
+
+func readDir(path, indent string) {
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, e := range entries {
+		info, err := e.Info()
+		if err != nil {
+			continue
+		}
+		t := "file"
+		size := fmt.Sprintf("(%db)", info.Size())
+
+		if e.IsDir() {
+			t = "dir"
+			size = "-"
+		}
+		fmt.Printf("%s %-10s %-5s %s\n", indent, e.Name(), t, size)
+	}
+}
+
+func main() {
+	path := flag.String("path", ".", "default")
+	r := flag.Bool("r", false, "recursion")
+	flag.Parse()
+	if *r {
+		printDirRecursive(*path, "")
+	} else {
+		readDir(*path, "")
 	}
 }
